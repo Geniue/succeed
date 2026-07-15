1761c000fcaef1fa85aa20bcd30d4b075285be52
ahmedatcs50 <ahmedatcs50@gmail.com>
2026-07-15 23:05:27 +0300

    Add pages/{slug} API endpoint; remove stray tarball from repo

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\ContactSubmissionController;
+use App\Http\Controllers\Api\V1\PageController;
 use App\Http\Controllers\Api\V1\SiteController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,9 @@ Route::prefix('v1')->name('api.v1.')->group(function () {
 
     Route::get('/site/bootstrap', [SiteController::class, 'bootstrap'])
         ->name('site.bootstrap');
+
+    Route::get('/pages/{slug}', [PageController::class, 'show'])
+        ->name('pages.show');
 
     Route::post('/contact-submissions', [ContactSubmissionController::class, 'store'])
         ->middleware('throttle:contact')
